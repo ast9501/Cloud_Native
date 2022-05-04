58f432303e3874ab8ca909c1ef41fcaeb5c7f5b2
vagrant <vagrant@vagrant.vm>
2022-05-04 13:06:13 +0000

    Refactor code

--- apps/bowling/src/app/game.ts
+++ apps/bowling/src/app/game.ts
@@ -10,13 +10,13 @@ export class Game {
       let score = 0;
       let frameIndex = 0;
       for (let frame = 0; frame < 10; frame++) {
-        if (this.rolls[frameIndex] === 10) {
-            score += 10 + this.rolls[frameIndex + 1] + this.rolls[frameIndex + 2];
+        if (this.isStrike(frameIndex)) {
+            score += 10 + this.strikeBonus(frameIndex);
             frameIndex++;
             continue;
         }
         if (this.isSpare(frameIndex)) {
-          score += 10 + this.rolls[frameIndex + 2];
+          score += 10 + this.spareBonus(frameIndex);
         } else {
           score += this.rolls[frameIndex] + this.rolls[frameIndex + 1];
         }
@@ -27,5 +27,15 @@ export class Game {
 
     private isSpare(frameIndex: number) {
         return this.rolls[frameIndex] + this.rolls[frameIndex + 1] === 10;
+    }
+    private isStrike(frameIndex: number){
+        return this.rolls[frameIndex] === 10;
+    }
+    private strikeBonus(frameIndex: number){
+        return this.rolls[frameIndex + 1] + this.rolls[frameIndex + 2];
+    }
+
+    private spareBonus(frameIndex: number){
+        return this.rolls[frameIndex + 2];
     }
 }
